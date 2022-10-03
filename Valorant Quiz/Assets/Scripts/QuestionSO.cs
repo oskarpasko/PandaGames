83a81408da53cecbae1067a5d62faa4dcacbb8f2
Oskar <>
2022-10-03 21:29:53 +0200

    Add logic to questions

--- Valorant Quiz/Assets/Scripts/QuestionSO.cs
+++ Valorant Quiz/Assets/Scripts/QuestionSO.cs
@@ -7,10 +7,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
     }
 
 }
