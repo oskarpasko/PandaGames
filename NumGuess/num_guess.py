da21c78c515f83ee67c09b3341a7751f18768584
Oskar <>
2022-10-11 19:58:23 +0200

    Update num_guess.py

--- NumGuess/num_guess.py
+++ NumGuess/num_guess.py
@@ -1,1 +1,16 @@
-print(' ----- Welcome in NumGuess ----- ')
+print(' ----- Welcome in NumGuess ----- \n\n')
+
+print('Choose Your option')
+print('1. Play\n2. Rules\n3. Exit')
+
+choice = input()
+
+match(choice):
+    case '1':
+        print("Start playing")
+    case '2':
+        print("RULES!")
+    case '3':
+        print("Exit from game")
+    case _:
+        print("Error 890!")
