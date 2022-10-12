85a94c50e0e69ea3b9bf534534bf4a1acca62781
Oskar <>
2022-10-12 10:14:01 +0200

    create mechanic for base game

--- NumGuess/game.py
+++ NumGuess/game.py
@@ -1,2 +1,20 @@
+import random
+
 def game(min_num, max_num, attempt):
-    pass
+    random_num = random.randint(min_num, max_num)
+
+    for x in range(0, attempt):
+        guess = input('Guess Number!')
+        guess = int(guess)
+
+        if(guess == random_num):
+            print('Bravo, this is number which You had to guess!')
+            break
+        elif(guess > random_num):
+            print('To much')
+        else:
+            print('Not enough!')
+
+        if(x == 3):
+            print('This is Your last chance!')
+        
