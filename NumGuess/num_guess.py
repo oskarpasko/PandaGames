105e775855037ca609bec485bf77f3e8d5c41b26
Oskar <>
2022-10-12 08:07:36 +0200

    import files and change menu code

--- NumGuess/num_guess.py
+++ NumGuess/num_guess.py
@@ -1,3 +1,5 @@
+import game, rules
+
 print(' ----- Welcome in NumGuess ----- \n\n')
 
 print('Choose Your option')
@@ -7,10 +9,14 @@ choice = input()
 
 match(choice):
     case '1':
-        print("Start playing")
+        game(0, 10, 5)
     case '2':
-        print("RULES!")
+        game(0, 100, 10)
     case '3':
+        game(0, 1000, 20)
+    case '4':
+        print("RULES!")
+    case '5':
         print("Exit from game")
     case _:
         print("Error 890!")
