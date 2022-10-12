e129bf49c0a63de682f46389c801ef942d140b9e
Oskar <>
2022-10-12 10:14:25 +0200

    bugfix

--- NumGuess/num_guess.py
+++ NumGuess/num_guess.py
@@ -9,14 +9,14 @@ choice = input()
 
 match(choice):
     case '1':
-        game(0, 10, 5)
+        game.game(0, 10, 5)
     case '2':
-        game(0, 100, 10)
+        game.game(0, 100, 10)
     case '3':
-        game(0, 1000, 20)
+        game.game(0, 1000, 20)
     case '4':
         print("RULES!")
     case '5':
-        print("Exit from game")
+        exit()
     case _:
         print("Error 890!")
