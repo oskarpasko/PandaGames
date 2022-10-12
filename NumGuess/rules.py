28cffb8e968ebe1e88fe9a1bd289e4695856fb5f
Oskar <>
2022-10-12 10:20:59 +0200

    create rules

---
+++ NumGuess/rules.py
@@ -0,0 +1,8 @@
+def rules():
+    rules_text = """
+    On the Easy level you have 5 attempt to guess number from 0 to 10.
+    On the Medium level you have 10 attempt to guess number from 0 to 100.
+    On the Hard level you have 20 attempt to guess number from 0 to 1000.
+    """
+
+    print(rules_text)
