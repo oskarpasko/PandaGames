9425bd8ef234e725fc54f2d259e592be812104f4
Oskar <>
2022-10-17 14:16:57 +0200

    Update num_guess.py

--- NumGuess/num_guess.py
+++ NumGuess/num_guess.py
@@ -6,45 +6,47 @@ extra_level_path = os.path.exists('dlc/extra_level.py')
 
 print(' ----- Welcome in NumGuess ----- \n\n')
 
-if(extra_level_path == False):
-    print('Choose Your option')
-    print('1. Play(Easy)\n2. Play(MEdium)\n3. Play(Hard)\n4. Rules\n5. Exit')
+while(True):
 
-    choice = input()
+    if(extra_level_path == False):
+        print('Choose Your option')
+        print('1. Play(Easy)\n2. Play(Medium)\n3. Play(Hard)\n4. Rules\n5. Exit')
 
-    match(choice):
-        case '1':
-            game.game(0, 10, 5)
-        case '2':
-            game.game(0, 100, 10)
-        case '3':
-            game.game(0, 1000, 20)
-        case '4':
-            rules.rules()
-        case '5':
-            exit()
-        case _:
-            print("Error 890!")
-else:
+        choice = input()
 
-    from .dlc import extra_level
-    print('Choose Your option')
-    print('1. Play(Easy)\n2. Play(MEdium)\n3. Play(Hard)\n4. Play(Custom)\n5. Rules\n6. Exit')
+        match(choice):
+            case '1':
+                game.game(0, 10, 5)
+            case '2':
+                game.game(0, 100, 10)
+            case '3':
+                game.game(0, 1000, 20)
+            case '4':
+                rules.rules()
+            case '5':
+                exit()
+            case _:
+                print("Error 890!")
+    else:
 
-    choice = input()
+        from dlc import extra_level
+        print('Choose Your option')
+        print('1. Play(Easy)\n2. Play(MEdium)\n3. Play(Hard)\n4. Play(Custom)\n5. Rules\n6. Exit')
 
-    match(choice):
-        case '1':
-            game.game(0, 10, 5)
-        case '2':
-            game.game(0, 100, 10)
-        case '3':
-            game.game(0, 1000, 20)
-        case '4':
-            extra_level.extra_level_game()
-        case '5':
-            rules.rules()
-        case '6':
-            exit()
-        case _:
-            print("Error 890!")
+        choice = input()
+
+        match(choice):
+            case '1':
+                game.game(0, 10, 5)
+            case '2':
+                game.game(0, 100, 10)
+            case '3':
+                game.game(0, 1000, 20)
+            case '4':
+                extra_level.extra_level_game()
+            case '5':
+                rules.rules()
+            case '6':
+                exit()
+            case _:
+                print("Error 890!")
