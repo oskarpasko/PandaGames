db849a1bda97768707bf63f3b4e90430fb6bf093
Oskar <>
2022-10-17 18:59:32 +0200

    Add mechanics to choose dlc to play

--- NumGuess/dlc.py
+++ NumGuess/dlc.py
@@ -1,38 +1,35 @@
-from dlcs import *
+from dlcs import extra_level, codeguess, pvp
+import num_guess
 import os.path
 
 # file_exists = os.path.exists('readme.txt')
 
 def dlc():
-    dlcs = ['extra_level.py', 'codeguess.py', 'pvp.py']
-    existed_dlc = []
-
-    for dlc in dlcs:
-        if os.path.exists('dlcs/'+dlc):
-            existed_dlc.append(dlc)
-
     print('\n ------------------------------- ')
     print(' ---------- Your DLC ----------- ')
     print(' ------------------------------- \n')
 
-    for dlc in range(len(existed_dlc)):
-        print(f"{dlc+1}. {_convert(existed_dlc[dlc])}")
+    while(True):
+        print('1. Play(Custom)\n2. CodeGuess\n3. PvP\n4. Back')
+        choice = input('What do You choose?\n')
 
-    choice = input('\n Which one You wanna play?\n')
-
-
-
-
-
-def _convert(dlc):
-    match(dlc):
-        case 'extra_level.py':
-            return "Play (Custom)"
-        case 'codeguess.py':
-            return "CodeGuess"
-        case 'pvp.py':
-            return "PvP"
-        case _:
-            print("Error 400!")
-
-dlc()
+        match(choice):
+            case '1':
+                if(os.path.exists('dlcs/extra_level.py')):
+                    extra_level.extra_level_game()
+                else:
+                    print('Error 1')
+            case '2':
+                if(os.path.exists('dlcs/codeguess.py')):
+                    codeguess.codeguess_game()
+                else:
+                    print('Error 1')
+            case '3':
+                if(os.path.exists('dlcs/pvp.py')):
+                    pvp.pvp_game()
+                else:
+                    print('Error 1')
+            case '4':
+                num_guess.numguess_game()
+            case _:
+                print('Error 890')
